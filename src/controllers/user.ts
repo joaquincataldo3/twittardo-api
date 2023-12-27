447c491e822459a60f1b793a2ec8375ad141bc72
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-12-27 08:15:36 -0300

    testing cookies

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -182,7 +182,7 @@ const controller = {
             userVerified.image_url = imageUrl;
             const token = jwt.sign({ ...userVerified }, secretKey);
             res.cookie('user_access_token', token, { httpOnly: true, secure: false });
-            console.log(req.cookies.user_access_token)
+            console.log({cookie: req.cookies })
             req.session.userLogged = userVerified;
 
             return res.status(200).json({ userVerified, token })
